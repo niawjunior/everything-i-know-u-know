ccf2c1a260c96948883a4803c2f55098f520497c
niawjunior <niawkung@gmail.com>
2023-12-03 13:02:43 +0700

    feat: react-with-panda-css

---
+++ react-with-panda-css/src/App.tsx
@@ -0,0 +1,13 @@
+import { css } from "../styled-system/css"
+
+function App() {
+  return (
+    <>
+      <div className={css({ fontSize: "7xl", fontWeight: "bold" })}>
+        React with Panda CSS 🐼!
+      </div>
+    </>
+  )
+}
+
+export default App
